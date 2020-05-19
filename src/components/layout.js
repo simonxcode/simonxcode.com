e1d0071ec31f7330aabb34f2fa2470ea8bb942aa
Simon X <simonxiong11@gmail.com>
2020-05-18 21:05:54 -0700

    Center all content in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Header from './header'
 import Footer from './footer'
 import '../styles/index.scss'
+import layoutModule from './layout.module.scss'
 
 const Layout = (props) => {
   return (
-    <div>
+    <div className={layoutModule.container}>
       <Header />
       {props.children}
       <Footer />
