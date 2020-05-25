3a15a39c31d672a587723d860901c2f119407a18
Simon X <simonxiong11@gmail.com>
2020-05-24 19:05:08 -0700

    Include image links for all projects

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -15,7 +15,9 @@ const ProjectPage = () => {
           </p>
           <p>This app was built with React, Redux and styled with Sass.</p>
           <a href="https://github.com/simonxcode/scoreKeeper-redux" target="_blank">Repository</a>
-          <a><p>Image of Project</p></a>
+          <a href="https://scorekeeper-app.surge.sh/" target="_blank">
+            <img src="../images/scoreKeeper.jpg" alt="screenshot of Score Keeper" />
+          </a>
         </div>
         <div>
           <h2>Chirpper</h2>
@@ -24,7 +26,9 @@ const ProjectPage = () => {
           </p>
           <p>This app was built with JavaScript, Node, Express, MongoDB and styled with CSS</p>
           <a href="https://github.com/chingu-voyage5/Geckos-Team-16" target="_blank">Repository</a>
-          <a><p>Image of Project</p></a>
+          <a href="https://chingu-chirpper.herokuapp.com/timeline/chingu" target="_blank">
+            <img src="../images/chirpper.jpg" alt="screenshot of Chirpper" />
+          </a>
         </div>
         <div>
           <h2>In the Pan</h2>
@@ -33,7 +37,9 @@ const ProjectPage = () => {
           </p>
           <p>This application with build with Vue, VueX, Vuetify and utilized a 3rd party API.</p>
           <a href="https://github.com/chingu-voyages/v8-geckos-team-11" target="_blank">Repository</a>
-          <a><p>Image of Project</p></a>
+          <a href="https://inthepan-app.surge.sh/" target="_blank">
+            <img src="../images/inThePan.jpg" alt="screenshot of In the Pan" />
+          </a>
         </div>
         <div>
           <h2>Tic Tac Toe</h2>
@@ -41,8 +47,10 @@ const ProjectPage = () => {
           The classic paper-and-pencil game where players have to match 3 in-a-row to win.
           </p>
           <p>This application was built with React and styled with Material-UI</p>
-          <a href="https://github.com/simonxcode/tictactoe-react">Repository</a>
-          <a><p>Image of Project</p></a>
+          <a href="https://github.com/simonxcode/tictactoe-react" target="_blank">Repository</a>
+          <a href="https://simonxcode.github.io/tictactoe-react/" target="_blank">
+            <img src="../images/ticTacToe.jpg" alt="screenshot of Tic Tac Toe" />
+          </a>
         </div>
         
       </Layout>
