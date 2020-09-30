82e650004e6cd436301039927368f2a6f7cc7827
Simon X <simonxiong11@gmail.com>
2020-09-30 19:57:25 -0400

    Remove unnecessary files and folders

--- src/components/header.js
+++ src/components/header.js
@@ -18,7 +18,6 @@ const Header = () => {
         <Link className={headerStyles.brand} to="/">
           {data.site.siteMetadata.title}
           <p className={headerStyles.tagline}>always a student</p>
-          <p className={headerStyles.tagline}>#100DaysOfCode</p>
         </Link>
         </h1>
       <nav>
--- src/posts/unit-testing.md
+++
@@ -1,6 +0,0 @@
----
-title: "Unit Testing With Enzyme and Jest"
-date: "2020-06-13"
----
-
-In the blog you will learn how to unit test React component.
