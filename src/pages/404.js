600c60a50b532b73f6dcca1ea050f4506560d817
Simon X <simonxiong11@gmail.com>
2020-10-14 19:27:07 -0400

    Standardize padding for each page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
 import Layout from '../components/layout'
 import Head from '../components/head'
 
 const NotFound = () => {
   return (
     <Layout>
-      <Head title="404 page not found"/>
-      <h1>page not found</h1>
+      <Head title="404 - Page not found"/>
+      <h1>404 - Page not found</h1>
       <p><Link to="/">back to home</Link></p>
     </Layout>
   )
