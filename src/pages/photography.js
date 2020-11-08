bb05048ad2a64a16b02f201481eba019d9b6fcc7
Simon X <simonxiong11@gmail.com>
2020-11-07 19:40:14 -0500

    Change padding for Photography title

--- src/pages/photography.js
+++ src/pages/photography.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Head from '../components/head'
+import photographyStyle from './photography.module.scss'
 
 const PhotographyPage = () => {
   return (
     <div>
       <Layout>
         <Head title="photography"/>
-        <h1>Photography</h1>
-        <p>Bits and pieces of my photos</p> 
+        <p className={photographyStyle.title}>Photography</p>
+        <h1>Bits and pieces of my photos</h1> 
       </Layout>
   </div>
   )
