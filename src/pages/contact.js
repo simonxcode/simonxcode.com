938ad41d2599a40e124ed1eb2b9608d30a661258
Simon X <simonxiong11@gmail.com>
2020-11-07 19:43:14 -0500

    Change padding from Contact title

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Head from '../components/head'
+import contactStyle from './contact.module.scss'
 
 const ContactPage = () => {
   return (
     <div>
       <Layout>
         <Head title="contact" />
-        <h1>Contact</h1>
-        <p>Drop a line or just say hello</p>
+        <p className={contactStyle.title}>Contact</p>
+        <h1>Drop a line or just say hello</h1>
         <a href="https://twitter.com/simonxcode">Lets get social</a>
       </Layout>
     </div>
