dad990fb49cacde2d341beb18cd62c3410ed64b5
Simon X <simonxiong11@gmail.com>
2020-11-07 19:37:27 -0500

    Add summary to About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,15 +2,19 @@ import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Head from '../components/head'
+import aboutStyle from './about.module.scss'
 
 const AboutPage = () => {
   return (
     <div>
       <Layout>
         <Head title="about"/>
-        <h1>About Me</h1>
-        <p>Bio will show up here later on.</p>
-        <p><Link to="/contact">Want to work with me? React out.</Link></p>
+        <p className={aboutStyle.title}>About Me</p>
+        <h1>Full Stack Developer based in Atlanta, Georgia.</h1>
+        <p>Experienced in JavaScript based frameworks for building elegant 
+        and functional web applications. When not building software,
+        I enjoy partaking in photography, fitness and learning about other cultures.</p>
+        <p>Want to work with me?<Link to="/contact"> React out.</Link></p>
       </Layout>
     </div>
   )
