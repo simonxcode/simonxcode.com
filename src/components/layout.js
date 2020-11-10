252a93f1947b618a8a710dd2d6108f46b9b5f631
Simon X <simonxiong11@gmail.com>
2020-11-09 20:36:53 -0500

    Install and mock-up react-burger-menu

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import Header from './header'
-import Menu from './menu'
+// import Menu from './menu'
 import '../styles/index.scss'
 import layoutStyle from './layout.module.scss'
+import Sidebar from './sidebar'
 
 const Layout = (props) => {
   return (
     <div className={layoutStyle.container}>
+      <Sidebar />
        <div className={layoutStyle.sidebar}>
         <Header />
       </div>
       <div className={layoutStyle.content}>
         {props.children}
       </div>
-      <Menu />
+      {/* <Menu /> */}
     </div>
   )
 }
---
+++ src/components/sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { slide as Menu } from 'react-burger-menu'
+import './sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <Menu>
+      <p>Hello World</p>
+    </Menu>
+  )
+}
+
+export default Sidebar 
