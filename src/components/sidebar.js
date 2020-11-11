981b472f43890ecfdc8a9635461a4f3926c4c605
Simon X <simonxiong11@gmail.com>
2020-11-11 18:35:54 -0500

    Add link to Homepage in  sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import { slide as Menu } from 'react-burger-menu'
+import headerStyles from './header.module.scss'
 import './sidebar.css'
 
 const Sidebar = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          author
+        }
+      }
+    }
+  `)
   return (
     <Menu>
-      <p>Hello World</p>
+      <header className={headerStyles.header}>
+        <Link className={headerStyles.brand} to="/">
+          {data.site.siteMetadata.title}
+        </Link>
+      </header>
     </Menu>
   )
 }
