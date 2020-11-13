06cc531b94d685dbdce16d260f2026c1f0a171dd
Simon X <simonxiong11@gmail.com>
2020-11-12 20:41:19 -0500

    Render profile image on About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Head from '../components/head'
 import aboutStyle from './about.module.scss'
+import Img from 'gatsby-image'
 
 const AboutPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "profile.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1280) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  if(!data?.placeholderImage?.childImageSharp?.fluid) {
+    return <div>Picture not found</div>
+  }
+
   return (
     <div>
       <Layout>
@@ -16,6 +33,7 @@ const AboutPage = () => {
         I enjoy partaking in photography, fitness and learning about other cultures.</p>
         <p>Want to work with me?<Link to="/contact"> React out.</Link></p>
       </Layout>
+      <Img fluid={data.placeholderImage.childImageSharp.fluid}/>
     </div>
   )
 }
