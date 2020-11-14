7d70c1b4aabb6e9188a4cf2ed30475e6d9ec1e2b
Simon X <simonxiong11@gmail.com>
2020-11-13 21:25:01 -0500

    Render Github icon

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Layout from '../components/layout'
 import Head from '../components/head'
 import contactStyle from './contact.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons' 
 
 const ContactPage = () => {
   return (
@@ -11,6 +13,9 @@ const ContactPage = () => {
         <p className={contactStyle.title}>Contact</p>
         <h1>Drop a line or just say hello</h1>
         <a href="https://twitter.com/simonxcode">Lets get social</a>
+        <div>
+          <FontAwesomeIcon icon={faGithub} className={contactStyle.icon}/>
+        </div>
       </Layout>
     </div>
   )
