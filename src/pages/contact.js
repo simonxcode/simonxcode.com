71612377737d2969c89548a1483fcbb88f8b0b2f
Simon X <simonxiong11@gmail.com>
2020-11-14 13:01:55 -0500

    Enable animation effect to icons

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import Layout from '../components/layout'
 import Head from '../components/head'
 import contactStyle from './contact.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons' 
+import { faLinkedinIn, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons' 
 
 const ContactPage = () => {
   return (
@@ -12,11 +12,12 @@ const ContactPage = () => {
         <Head title="contact" />
         <p className={contactStyle.title}>Contact</p>
         <h1>Drop a line or just say hello</h1>
-        <a href="https://twitter.com/simonxcode">Lets get social</a>
-        <div>
+        <p>Now let's get social. <a href='https://mail.google.com/mail/?view=cm&fs=1&to=simonxiong11@gmail.com'>Emails</a> are also cool.</p>
+        <div className={contactStyle.social}>
           <ul>
-            <li><a href='https://www.linkedin.com/in/simonxcode/'><FontAwesomeIcon icon={faLinkedinIn} className={contactStyle.icon} /></a></li>
-            <li><a href='https://github.com/simonxcode'><FontAwesomeIcon icon={faGithub} className={contactStyle.icon} /></a></li>
+            <li><a href='https://github.com/simonxcode'><FontAwesomeIcon icon={faGithub} className={contactStyle.icon}/></a></li>
+            <li><a href='https://www.linkedin.com/in/simonxcode/'><FontAwesomeIcon icon={faLinkedinIn} className={contactStyle.icon}/></a></li>
+            <li><a href='https://twitter.com/simonxcode'><FontAwesomeIcon icon={faTwitter} className={contactStyle.icon}/></a></li>
           </ul>
         </div>
       </Layout>
