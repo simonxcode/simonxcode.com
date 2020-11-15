66a7c250b46a8efba8d6c383571a0aae00f57de3
Simon X <simonxiong11@gmail.com>
2020-11-15 17:51:40 -0500

    Add social icons to footer in tablet view

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import footerStyle from './footer.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons' 
 
 const Footer  = () => {
   const data = useStaticQuery(graphql`
@@ -14,6 +16,13 @@ const Footer  = () => {
   `)
   return (
     <div className={footerStyle.footer}>
+      <div className={footerStyle.social}>
+        <ul>
+          <li><a href='https://github.com/simonxcode'><FontAwesomeIcon icon={faGithub} className={footerStyle.icon} /></a></li>
+          <li><a href='https://www.linkedin.com/in/simonxcode/'><FontAwesomeIcon icon={faLinkedinIn} className={footerStyle.icon} /></a></li>
+          <li><a href='https://twitter.com/simonxcode'><FontAwesomeIcon icon={faTwitter} className={footerStyle.icon} /></a></li>
+        </ul>   
+      </div>
       <p>{data.site.siteMetadata.author} © 2020</p>
     </div>
   )
