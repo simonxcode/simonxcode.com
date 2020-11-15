45b3bb036248f97b2d43a8c0189fda099e7da969
Simon X <simonxiong11@gmail.com>
2020-11-15 16:54:49 -0500

    Style and properly align footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import footerStyle from './footer.module.scss'
 
 const Footer  = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `)
   return (
-    <div>
-      <p>This is the footer</p>
+    <div className={footerStyle.footer}>
+      <p>{data.site.siteMetadata.author} © 2020</p>
     </div>
   )
 }
