e5521477373e50fd7ea850b857f5797b0b4dbb01
Simon X <simonxiong11@gmail.com>
2021-05-15 21:52:54 -0400

    Change styling to ES Module import for Index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
-import indexStyle from './index.module.scss' 
+import { greeting, summary, link } from './index.module.scss' 
 import Head from '../components/head'
 
 const HomePage = () => {
@@ -11,13 +11,13 @@ const HomePage = () => {
         <Head title="home" />
         <div>
           <div>
-            <p className={indexStyle.greeting}>Hello, I 'm Simon.</p>
+            <p className={greeting}>Hello, I 'm Simon.</p>
           </div>
           <div>
             <h1>I like to help build impactful solutions for humans.</h1>
           </div>
-          <div className={indexStyle.summary}>
-            <p>Feel free to checkout my <Link className={indexStyle.link} to="/portfolio">portfolio</Link> to see my latest work.</p>
+          <div className={summary}>
+            <p>Feel free to checkout my <Link className={link} to="/portfolio">portfolio</Link> to see my latest work.</p>
           </div>
         </div>
       </Layout>
