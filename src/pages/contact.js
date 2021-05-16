78801f2136fc1a6b5b1d58e0bcb46b8faa5a6a87
Simon X <simonxiong11@gmail.com>
2021-05-16 13:37:17 -0400

    Change styling to ES Module for Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Head from '../components/head'
-import contactStyle from './contact.module.scss'
+import { title, social, icon } from './contact.module.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons' 
 
@@ -10,15 +10,15 @@ const ContactPage = () => {
     <div>
       <Layout>
         <Head title="contact" />
-        <p className={contactStyle.title}>Contact</p>
+        <p className={title}>Contact</p>
         <h1>Drop a line or just say hello.</h1>
         <p>Now lets get social.<a href='https://mail.google.com/mail/?view=cm&fs=1&to=simonxiong11@gmail.com'> Emails</a> are cool also.</p>
       
-        <div className={contactStyle.social}>
+        <div className={social}>
           <ul>
-            <li><a href='https://www.github.com/simonxcode'><FontAwesomeIcon icon={faGithub} className={contactStyle.icon}/></a></li>
-            <li><a href='https://www.linkedin.com/in/simonxcode/'><FontAwesomeIcon icon={faLinkedinIn} className={contactStyle.icon}/></a></li>
-            <li><a href='https://www.twitter.com/simonxcode'><FontAwesomeIcon icon={faTwitter} className={contactStyle.icon}/></a></li>
+            <li><a href='https://www.github.com/simonxcode'><FontAwesomeIcon icon={faGithub} className={icon}/></a></li>
+            <li><a href='https://www.linkedin.com/in/simonxcode/'><FontAwesomeIcon icon={faLinkedinIn} className={icon}/></a></li>
+            <li><a href='https://www.twitter.com/simonxcode'><FontAwesomeIcon icon={faTwitter} className={icon}/></a></li>
           </ul>
         </div>
       </Layout>
