d0c5c1a282b6f179104d5e1e5692e83958b11bff
Simon X <simonxiong11@gmail.com>
2021-05-23 18:48:48 -0400

    Remove Contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,14 +23,6 @@ module.exports = {
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-      }
-    },
     'gatsby-plugin-sass',
     {
       resolve: 'gatsby-source-filesystem',
