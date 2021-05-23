93d4ba1231a27087532207e1d99a015a5d60e229
Simon X <simonxiong11@gmail.com>
2021-05-23 18:29:39 -0400

    Remove markdown plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,19 +38,6 @@ module.exports = {
           name: 'src',
           path: `${__dirname}/src/`
         },
-        resolve: 'gatsby-transformer-remark',
-        options: {
-          plugins: [
-            'gatsby-remark-relative-images',
-            {
-              resolve: 'gatsby-remark-images',
-                options: {
-                  maxWidth: 750, 
-                  linkImagesToOriginal: false
-                }
-            }
-          ]
-        }
       }
   ],
 }
