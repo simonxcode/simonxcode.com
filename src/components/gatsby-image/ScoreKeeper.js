b5d138e547f49e4d85e4dc8aeacb26a46cc2a403
simonxcode <simonxiong11@gmail.com>
2022-05-16 01:01:36 -0400

    update node modules

--- src/components/gatsby-image/ScoreKeeper.js
+++ src/components/gatsby-image/ScoreKeeper.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { gatsbyImage } from '../../pages/portfolio.module.scss'
 
