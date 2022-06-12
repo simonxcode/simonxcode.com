34fb2c9af4f0be462c2d0a2861e9fc352e013f04
simonxcode <simonxiong11@gmail.com>
2022-06-11 21:06:54 -0400

    render rich text and image

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,28 +1,55 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { BLOCKS, MARKS } from '@contentful/rich-text-types'
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 
 export const query = graphql`
   query($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
       title
-      publishedDate(formatString: "MMMM Do, YYYY")
+      publishedDate(formatString: "MMM Do, YYYY")
       body {
         raw
+        references {
+          ... on ContentfulAsset {
+            contentful_id
+            title
+            gatsbyImageData(width: 1600)
+            __typename
+          }
+        }
       }
     }
   }
 `
 
-const BlogPost = (props) => {
+const BlogPost = ({data}) => {
+
+  const options = {
+    renderMark: {
+      [MARKS.BOLD]: text => <b>{text}</b>
+    },
+    renderNode: {
+      [BLOCKS.HEADING_1]: (node, children) => {
+        return <h1>{children}</h1>
+      },
+      [BLOCKS.EMBEDDED_ASSET]: node => {
+        const { gatsbyImageData, title} = node.data.target
+        return <GatsbyImage image={getImage(gatsbyImageData)} alt={title} />
+      }
+    }
+  }
+
   return (
     <Layout>
-      <h1>{props.data.contentfulBlogPost.title}</h1>
-      <p>{props.data.contentfulBlogPost.publishedDate}</p>
-      {documentToReactComponents(JSON.parse(props.data.contentfulBlogPost.body.raw))}
+      <h1>{data.contentfulBlogPost.title}</h1>
+      <p>{data.contentfulBlogPost.publishedDate}</p>
+      {renderRichText(data.contentfulBlogPost.body, options)}
     </Layout>
   )
+
 }
 
 export default BlogPost
